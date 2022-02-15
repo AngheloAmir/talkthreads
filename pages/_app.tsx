2f536e4739af77656058ad1491a48fa8a4276f34
angheloamir <angheloamir@gmail.com>
2022-02-16 01:22:01 +0800

    0.5 navbar completed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,39 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import NavigationBar from '../components/navbar';
+import navbar from '../app/data/navbar.json';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavigationBar
-        brandpath='brandtitle.png'
-        items={[
-          {
-            name: 'Home',
-            icon: 'app/navbar/home.png',
-            path: '/'
-          },
-          {
-            name: 'Messages',
-            icon: 'app/navbar/message.png',
-            path: '/'
-          },
-          {
-            name: 'Notification',
-            icon: 'app/navbar/notification.png',
-            path: '/'
-          },
-          {
-            name: 'Freinds',
-            icon: 'app/navbar/freinds.png',
-            path: '/'
-          },
-          {
-            name: 'Setting',
-            icon: 'app/navbar/setting.png',
-            path: '/',
-          }
-        ]}
+        brandpath={navbar.brandpath}
+        items={navbar.items}
       />
       <Component {...pageProps} />
     </>
