3cdda0e453a81d0a649384dff4501c4fa0e93a1a
angheloamir <angheloamir@gmail.com>
2022-02-25 01:12:28 +0800

    0.12 Adding text with image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,48 @@ import type { NextPage } from 'next'
 import LandingPageContainer from '../components/HomePage/landing/landingpagecontainer';
 import BeamContainer from '../components/HomePage/beam/beamcontainer';
 
+
 const Home: NextPage = () => {
   return (
     <div>
       <LandingPageContainer />
       <BeamContainer />
 
-      <div>
-        <h3>3000 miliosn of people are enjoying our service!</h3>
+      <div style={{
+        position: 'relative',
+        border: '1px solid red'
+      }}>
+      <img
+        src='home/join.jpg'
+        style={{
+          width: '100vw',
+          height: '100vh',
+          
+          
+          
+      
+        }}
+      />
+      <div style={{
+        position: 'absolute',
+        top: '10vh',
+        left: '50vh',
+        border: '2px solid white',
+        backgroundColor: 'rgba(50,50,50, 0.5)',
+        color: 'white',
+        padding: '3rem',
+        borderRadius: '1rem'
+      }}>
+        <h1>JOIN NOW</h1>
+      <h3>3000 miliosn of people are enjoying our service!</h3>
         <h3>dont be shy and it is FREE! Start now</h3>
         <button>Register!</button>
       </div>
+        
+      </div>
+
+      
+
 
       <div style={{
         width: '100%',
